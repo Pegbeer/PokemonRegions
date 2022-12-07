d4400597545dafb6eac01287afbcc6b467b278ac
Josue <55509957+Pegbeer@users.noreply.github.com>
2022-12-07 15:50:21 -0600

    ➕ Added Facebook SDK

--- app/src/main/java/com/example/pokemonregions/MainActivity.kt
+++ app/src/main/java/com/example/pokemonregions/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.pokemonregions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ){ res ->
+        this.onSignInResult(res)
+    }
+
+    private val providers = arrayListOf(
+        AuthUI.IdpConfig.GoogleBuilder().build(),
+        AuthUI.IdpConfig.FacebookBuilder().build()
+    )
+
+    private val signInIntent = AuthUI.getInstance()
+        .createSignInIntentBuilder()
+        .setTheme(R.style.Theme_PokemonRegions)
+        .setAvailableProviders(providers)
+        .build()
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult?) {
+        if(result == null){
+            return
+        }
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        signInLauncher.launch(signInIntent)
     }
 }
